ce186df8a8c4f7b0685daffadf1e13a3cd119d09
zahra <zahra>
2017-05-02 15:15:25 +0430

    first commit

---
+++ java/com/exampel/omid/AppCompatActivity.java
@@ -0,0 +1,7 @@
+package com.exampel.omid;
+
+/**
+ * Created by Informatic on 3/4/2017.
+ */
+public class AppCompatActivity {
+}
---
+++ java/com/exampel/omid/MainActivity.java
@@ -0,0 +1,12 @@
+package com.exampel.omid;
+
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
